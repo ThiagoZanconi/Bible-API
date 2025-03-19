2a07b2a8ea1cf24672c2e479eddb2e6e4a8647bd
ThiagoZanconi <thiagzanc@gmail.com>
2025-03-19 10:33:05 -0300

    Config

--- obj/Debug/net9.0/MiProyectoBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/MiProyectoBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiProyectoBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6682604f8392cfd299ac96e33b17fc76d91abb6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiProyectoBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiProyectoBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
