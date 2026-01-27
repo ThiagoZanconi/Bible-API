72217b625667ea90c87bab58c5ab5ab167d17e9b
ThiagoZanconi <thiagzanc@gmail.com>
2026-01-27 13:53:15 -0300

    Deleted Seeders

--- controllers/CollectionController.cs
+++ controllers/CollectionController.cs
@@ -2,7 +2,6 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using MiProyectoBackend.database;
 using MiProyectoBackend.postgres_model;
 
 [ApiController]
